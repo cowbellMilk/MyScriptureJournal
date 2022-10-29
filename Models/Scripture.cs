66c8531a6c1371813821ef05c480d72e70aeaa5f
cowbellMilk <chukwuebube.glory@gmail.com>
2022-10-29 04:47:42 -0600

    project complete

---
+++ Models/Scripture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyScriptureJournal.Models
+{
+	public class Scripture
+	{
+        public int ID { get; set; }
+
+        [StringLength(20, MinimumLength = 4)]
+        [Required]
+        public string Book { get; set; } = string.Empty;
+
+        [StringLength(5)]
+        [Required]
+        public string Chapter { get; set; } = string.Empty;
+
+        [StringLength(10)]
+        [Required]
+        public string Verses { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(200)]
+        public string Notes { get; set; } = string.Empty;
+
+        [Display(Name = "Entry Date")]
+        [DataType(DataType.Date)]
+        public DateTime EntryDate { get; set; } = DateTime.Now;
+    }
+}
+
